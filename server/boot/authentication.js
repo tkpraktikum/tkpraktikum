33ddb49ea2e38472013e36ddf379c2aded341780
Tarek Auel <tarek.auel@gmail.com>
2016-05-11 12:24:34 +0200

    added role and containser for uploading files

--- server/boot/authentication.js
+++ server/boot/authentication.js
@@ -3,8 +3,9 @@ module.exports = function enableAuthentication(app) {
   app.enableAuth();
 
   var Role = app.models.Role;
+  var RoleMapping = app.models.RoleMapping;
 
-  Role.registerResolver('authorOwner', function(role, context, callback) {
+  Role.registerResolver('$authorOwner', function(role, context, callback) {
     if (!context || !context.model || !context.modelId) {
       process.nextTick(function() {
         if (callback) callback(null, false);
@@ -14,9 +15,21 @@ module.exports = function enableAuthentication(app) {
     var modelClass = context.model;
     var modelId = context.modelId;
     var userId = context.getUserId();
-    var user = context.user;
-    //user.
-    Role.isOwner(modelClass, modelId, userId, callback);
+
+    Role.isOwner(modelClass, modelId, userId, function(err, owner) {
+      if (err || !owner) {
+        return callback(err, owner);
+      }
+
+      RoleMapping
+        .findOne({where: {and: [{roleId: Role.AUTHOR.id}, { principalId: userId}]}})
+        .then(function(result) {
+          callback(null, result !== null);
+        })
+        .catch(function(err) {
+          callback(err);
+        });
+    });
   });
 
 };
