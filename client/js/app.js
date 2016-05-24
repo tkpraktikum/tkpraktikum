90f1fd25f8281f7bc0c3d58e9d9e9dca8b15cd5d
Markus Schanz <coksnuss@googlemail.com>
2016-05-24 22:03:19 +0200

    Improved login handling (stay on page while login/logout) Amount of requests to get user roles is now reduced to a minimum. Improved role and permission checks (views are now updated accordingly when state changes)

--- client/js/app.js
+++ client/js/app.js
@@ -13,7 +13,7 @@ angular
         name: "app",
         abstract: true,
         url: "/",
-        template: '<ui-view />',
+        templateUrl: 'views/layout.html',
         children: [{
           name: "public",
           template: '<ui-view />',
@@ -23,6 +23,10 @@ angular
             url: 'login',
             templateUrl: 'views/login.html',
             controller: 'LoginController'
+          }, {
+            name: 'logout',
+            url: 'logout',
+            controller: 'LogoutController'
           }, {
             name: 'signup',
             url: 'signup',
@@ -82,115 +86,68 @@ angular
       });
     $urlRouterProvider.otherwise('tag');
   }])
-  .factory("$authentication", ["$rootScope", 'ipCookie', function ($rootScope, ipCookie) {
-
-    var cookieName = "statusLoggedIn";
-    var cookieNameId = "statusUserId";
-
-    return {
-
-      clearAuthKey: function () {
-        return ipCookie.remove(cookieName, {
-          path: "/"
+.factory('AuthService', ['$q', 'LoopBackAuth', 'User', function ($q, LoopBackAuth, User) {
+  var user,
+    login = function (username, password, rememberMe) {
+      return user = User.login({
+          username: username,
+          password: password,
+          rememberMe: rememberMe || false
+        }).$promise.then(function (response) {
+          return retrieveUserWithRoles(response.user.id);
         });
-      },
-
-      getAuthKey: function () {
-        $rootScope.loggedIn = !!(ipCookie(cookieName));
-        if (ipCookie(cookieNameId)) {
-          $rootScope.userId = ipCookie(cookieNameId);
-        } else {
-          $rootScope.userId = -1;
-          $rootScope.userRoles = [];
-        }
-        return $rootScope.loggedIn;
-      },
-
-      setAuthKey: function (key) {
-        ipCookie(cookieName, key, {
-          path: "/"
-        });
-      }
-    };
-  }]).factory('loginStatusWatcher', ["$authentication", function ($authentication) {
-  return {
-    response: function (response) {
-      $authentication.getAuthKey();
-      return response;
-    }
-  };
-}]).config(['$httpProvider', function ($httpProvider) {
-  $httpProvider.interceptors.push('loginStatusWatcher');
-}]).run(function($rootScope, $http, $q, PermissionStore, RoleStore) {
-  PermissionStore.definePermission('hasValidSession', (function () {
-    var promise = $q(function (resolve, reject) {
-      $http({method: 'GET', url: '/auth/me'}).then(
-        function (rsp) { rsp.data && rsp.data.id > 0 ? resolve() : reject(); },
-        function (err) { reject(); }
-      );
-    });
-
-    $rootScope.$watch('loggedIn', function (loggedIn) {
-      // Unfortunately the angular promise API does not expose a status. So we
-      // need to rely on the internal implementation by accessing $$state.
-      // If the current promise is not resolved -> ignore state change.
-      if ([1,2].indexOf(promise.$$state.status) >= 0) {
-        promise = $q(function (resolve, reject) { loggedIn ? resolve() : reject() });
-      }
-    });
-
-    return function () {
-      return promise;
-    }
-  })());
-
-  var roleChecker = (function () {
-    var deferred = $q.defer(); // do not resolve until roles are ready
-
-    $rootScope.$watch('userRoles', function (userRoles) {
-      if (Object.prototype.toString.call(userRoles) === '[object Array]') {
-        // Promise was already resolved, create new promise
-        if ([1,2].indexOf(deferred.promise.$$state.status)) {
-          deferred = $q.defer();
-        }
-
-        // Resolve pending promise
-        deferred.resolve(userRoles);
-      }
-    });
-
-    return function (roleName, transitionProperties) {
-      return $q(function (resolve, reject) {
-        deferred.promise.then(
-          function (val) { val.indexOf(roleName.toLowerCase()) >= 0 ? resolve() : reject(); },
-          function (err) { reject(); }
-        );
+    },
+    logout = function () {
+      return User.logout().$promise.then(function () {
+        // LoopBackAuth.clearUser();
+        // LoopBackAuth.clearStorage();
+        user = rejectedPromise();
       });
-    }
-  })();
+    },
+    hasRole = function (role) {
+      return user.then(
+        function (user) { return user.roles.indexOf(role) !== -1 },
+        function () { return false; }
+      );
+    },
+    isAuthenticated = User.isAuthenticated.bind(User),
+    rejectedPromise = function () {
+      return $q.defer(function (resolve, reject) { reject(); }).promise;
+    },
+    retrieveUserWithRoles = function (userId) {
+      return User.findById({id: userId, filter: {include: 'roles'}})
+        .$promise.then(function (response) {
+          response.roles = response.roles.map(function (element) {
+            return element.name;
+          });
+          return response;
+        });
+    };
 
-  PermissionStore.defineManyPermissions(['chair', 'author', 'reviewer'], roleChecker);
+  user = isAuthenticated() ?
+    retrieveUserWithRoles(LoopBackAuth.currentUserId) :
+    rejectedPromise();
+
+  return {
+    login: login,
+    logout: logout,
+    hasRole: hasRole,
+    isAuthenticated: isAuthenticated
+  };
+}])
+.run(function($rootScope, $q, PermissionStore, RoleStore, AuthService) {
+  PermissionStore.definePermission('hasValidSession', function () {
+    console.log("ASK FOR VALID SESSION");
+    return AuthService.isAuthenticated();
+  });
+  PermissionStore.defineManyPermissions(['chair', 'author', 'reviewer'],
+      function (roleName, transitionProperties) {
+    console.log("ASK FOR PERMISSION");
+    return AuthService.hasRole(roleName);
+  });
+
   RoleStore.defineRole('USER', ['hasValidSession']);
   RoleStore.defineRole('CHAIR', ['hasValidSession', 'chair']);
   RoleStore.defineRole('AUTHOR', ['hasValidSession', 'author']);
   RoleStore.defineRole('REVIEWER', ['hasValidSession', 'reviewer']);
-
-  $rootScope.$watch('userId', function (userId) {
-    // get the roles of this user if the userId changes
-    if (userId !== undefined && userId > 0) {
-      $http({
-        method: 'GET',
-        url: '/api/users/' + userId + '/roles'
-      }).then(function successCallback(response) {
-        $rootScope.userRoles = response.data.map(function (e) {
-          return e.name
-        });
-        console.log($rootScope.userRoles);
-      }, function errorCallback(response) {
-        console.log(JSON.stringify(response));
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
-      });
-    }
-  });
 });
