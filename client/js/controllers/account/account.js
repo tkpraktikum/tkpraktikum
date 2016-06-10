f4bf270bbef2e7cf68caee61e1106cc647680ef6
Huzeyfe <huzeyfe169@hotmail.de>
2016-06-10 16:19:25 +0200

    edit profile view with no functions implemented

--- client/js/controllers/account/account.js
+++ client/js/controllers/account/account.js
@@ -7,6 +7,36 @@ angular
       $scope.user = userData;
     });
   }])
-  .controller('UserController', ['$scope', '$state', function($scope, $state) {
+  .controller('UserController', ['$scope', 'AuthService', 'Affiliation', function($scope, AuthService, Affiliation) {
+    $scope.user = {};
+    $scope.affiliations = [];
+
+    AuthService.getUser().then(function (userData) {
+      $scope.user = userData;
+    });
+
+
+    function getAffiliations() {
+      Affiliation
+        .find()
+        .$promise
+        .then(function(results) {
+          $scope.affiliations = results;
+        });
+    }
+
+    getAffiliations();
+
+    $scope.changeProfile = function () {
+
+    };
+
+    $scope.deleteProfile = function () {
+
+    };
+
+    $scope.changePassword = function () {
+
+    };
 
   }]);
