e3aeb87afb95dcbc630c612bbfc03ae2e05246dc
Huzeyfe <huzeyfe169@hotmail.de>
2016-06-11 07:26:32 +0200

    implemented new edit profile view (added temporary input-validation + responsive dropdown menu for affiliation (fills the affiliation name, city, state, country fields automatically with dropdown selection and dropdown-menu width is 100% with the width of column) -> (currently no save functionality for the fields) fixed some minor bugs in the login/signup view/controller and createAffiliations.js (state: "Hesse" -> "Hessen") added directive.js for checking two password fields (checkPassword) added padding-top for in the layout.html for the header view (in tablet mode, navbar needs to be fixed, hence padding-top:50px) changed/removed classes in styles.css

--- client/js/controllers/account/account.js
+++ client/js/controllers/account/account.js
@@ -1,15 +1,23 @@
 angular
   .module('app')
-  .controller('AccountController', ['$scope', 'AuthService', function($scope, AuthService) {
+  .controller('AccountController', ['$scope', 'AuthService', function ($scope, AuthService) {
+
     $scope.user = {};
 
     AuthService.getUser().then(function (userData) {
       $scope.user = userData;
     });
+
   }])
-  .controller('UserController', ['$scope', 'AuthService', 'Affiliation', function($scope, AuthService, Affiliation) {
+  .controller('UserController', ['$scope', 'AuthService', 'Affiliation', function ($scope, AuthService, Affiliation) {
+
     $scope.user = {};
     $scope.affiliations = [];
+    $scope.changeUserProfile = {};
+    $scope.changeUserPassword = {};
+    $scope.deleteUserProfile = {};
+    $scope.selectedItem = null;
+    $scope.changeCaret = false;
 
     AuthService.getUser().then(function (userData) {
       $scope.user = userData;
@@ -20,23 +28,32 @@ angular
       Affiliation
         .find()
         .$promise
-        .then(function(results) {
+        .then(function (results) {
           $scope.affiliations = results;
         });
     }
 
     getAffiliations();
 
-    $scope.changeProfile = function () {
+    $scope.dropDownItemSelected = function (selection) {
+      $scope.changeCaret = !$scope.changeCaret;
+      $scope.selectedItem = selection.name;
+      $scope.changeUserProfile.affiliation = selection.name;
+      $scope.changeUserProfile.city = selection.city;
+      $scope.changeUserProfile.state = selection.state;
+      $scope.changeUserProfile.country = selection.country;
+    };
 
+    $scope.changeProfile = function () {
+      console.log(JSON.stringify($scope.changeUserProfile));
     };
 
     $scope.deleteProfile = function () {
-
+      console.log(JSON.stringify($scope.changeUserPassword));
     };
 
     $scope.changePassword = function () {
-
+      console.log(JSON.stringify($scope.deleteUserProfile));
     };
 
   }]);
