a4be0b6d60116994cecf5f2019f71a62343acb7e
Huzeyfe <huzeyfe169@hotmail.de>
2016-08-23 15:17:04 +0200

    fixed loopback call

--- client/js/controllers/user/profile.view.js
+++ client/js/controllers/user/profile.view.js
@@ -1,18 +1,10 @@
 angular
   .module('app')
-  .controller('ProfileViewController', ['$scope', '$state', 'AuthService', 'Affiliation', 'User', '$stateParams', function ($scope, $state, AuthService, Affiliation, User, $stateParams) {
+  .controller('ProfileViewController', ['$scope', '$state', 'AuthService', 'User', '$stateParams', function ($scope, $state, AuthService, User, $stateParams) {
 
-    User.findById({id: $stateParams.userId, include: ['useraffiliation']}).$promise.then(function(u) {
+
+    User.findById({id: $stateParams.userId, filter: {include: ['affiliations']}}).$promise.then(function(u) {
       $scope.user = u;
-      if(u.affiliation == null)
-        return;
-      Affiliation.findById({id: u.affiliation, include: ['affiliation']}).$promise.then(function(a) {
-        $scope.user.affiliation = a;
-      }).catch(function() {
-        console.log('Affiliation data error!');
-      })
-    }).catch(function() {
-      console.log('User data error!');
     });
 
   }]);
