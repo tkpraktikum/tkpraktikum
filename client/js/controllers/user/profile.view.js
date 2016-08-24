46e0e54cb11bf222c7d9420649b801acc2e7e3fd
Tarek Auel <tarek@auel.io>
2016-08-24 14:38:15 +0200

    fixed view user-profile

--- client/js/controllers/user/profile.view.js
+++ client/js/controllers/user/profile.view.js
@@ -1,10 +1,33 @@
 angular
   .module('app')
-  .controller('ProfileViewController', ['$scope', '$state', 'AuthService', 'User', '$stateParams', function ($scope, $state, AuthService, User, $stateParams) {
+  .controller('ProfileViewController', ['$scope', '$state', 'AuthService', 'User', '$stateParams', 'uiGmapGoogleMapApi', 'uiGmapIsReady', function ($scope, $state, AuthService, User, $stateParams, uiGmapGoogleMapApi, IsReady) {
 
+    $scope.map = { control: {}, center: { latitude: 49.877613, longitude: 8.654847 }, zoom: 14 };
+
+    var refreshMap = function() {
+      if ($scope.ready && $scope.user && $scope.user.lat && $scope.user.lng) {
+        $scope.map.markers = [{
+          id: 'home',
+          coords: {
+            latitude: $scope.user.lat,
+            longitude: $scope.user.lng
+          }
+        }];
+        var ll = new google.maps.LatLng($scope.user.lat, $scope.user.lng);
+        $scope.map.control.getGMap().panTo(ll);
+      }
+    };
 
     User.findById({id: $stateParams.userId, filter: { include: ['affiliation'] } }).$promise.then(function(u) {
       $scope.user = u;
+      if (u.lat && u.lng) {
+        refreshMap();
+      }
+    });
+
+    IsReady.promise().then(function (maps) {
+      $scope.ready = true;
+      refreshMap();
     });
 
   }]);
