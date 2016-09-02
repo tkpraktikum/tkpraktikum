7f2ae7c99718e5c8f2150208c386e797825721f8
Tarek Auel <tarek@auel.io>
2016-09-02 19:09:13 +0200

    todos

--- client/js/controllers/chair/statistics.js
+++ client/js/controllers/chair/statistics.js
@@ -4,6 +4,10 @@ angular
     ['$q', '$scope', '$state', '$stateParams', '$http', '$timeout', 'ConferenceService', 'Submission', 'User', 'Conference', 'Review',
       function($q, $scope, $state, $stateParams, $http, $timeout, ConferenceService, Submission, User, Conference, Review) {
 
+        $scope.submissionPromise = Submission.find(
+          {filter: {where: { conferenceId: ConferenceService.getCurrentConferenceId()},
+          include: {'authors': ['affiliation']}}}).$promise;
+
         var authorVsSubmissions = function() {
           $scope.labels = ['Submissions'];
           $scope.series = ['Authors', 'Submissions'];
@@ -32,6 +36,42 @@ angular
         };
         authorVsSubmissions();
 
+        var users = function() {
+          $scope.labelsUser = ['Submissions'];
+          $scope.seriesUser = ['Attendees', 'Authors', 'Reviewers', 'Chairs'];
+
+          $scope.optionsUser = {
+            legend: {display: true},
+            scales : {
+              yAxes: [{
+                display: true,
+                ticks: {
+                  suggestedMin: 0
+                }
+              }]
+            }
+          };
+
+          $scope.dataUser = [[],[], [], []];
+
+          Conference.authors.count({id: ConferenceService.getCurrentConferenceId()}).$promise.then(function(result) {
+            $scope.dataUser[0].push(result.count);
+          });
+
+          Conference.attendees.count({id: ConferenceService.getCurrentConferenceId()}).$promise.then(function(result) {
+            $scope.dataUser[1].push(result.count);
+          });
+
+          Conference.reviewer.count({id: ConferenceService.getCurrentConferenceId()}).$promise.then(function(result) {
+            $scope.dataUser[2].push(result.count);
+          });
+
+          Conference.chairs.count({id: ConferenceService.getCurrentConferenceId()}).$promise.then(function(result) {
+            $scope.dataUser[3].push(result.count);
+          });
+        };
+        users();
+
         var reviewsFinishedVsDraft = function() {
           $scope.reviewsLabels = ["Finished", "Open"];
           $scope.reviewsData = [];
@@ -39,8 +79,7 @@ angular
             legend: {display: true}
           };
 
-          Submission.find({filter: {where: { conferenceId: ConferenceService.getCurrentConferenceId()}}}).$promise
-            .then(function(submission) {
+          $scope.submissionPromise.then(function(submission) {
               var ids = submission.map(function(s) { return {submissionId: s.id}; });
               Review.find({filter: {where: { or: ids}}}).$promise.then(function(r) {
                 $scope.reviewsData.push(r.filter(function(r) { return r.finished;}).length);
@@ -51,43 +90,61 @@ angular
         };
         reviewsFinishedVsDraft();
 
-        $scope.reviewsTimeLabels = ["January", "February", "March", "April", "May", "June", "July", "August", "September"];
-        $scope.reviewsTimeSeries = ['Series A'];
-        $scope.reviewsTimeData = [
-          [5, 8, 13, 20, 40, 80, 100, 106, 110]
-        ];
+        var submissionsByAffs = function() {
 
-        $scope.reviewsTimeOptions = {
-          scales: {
-            yAxes: [{
-              display: true,
-              ticks: {
-                suggestedMin: 0
-              }
-            }]
-          }
+          $scope.subAffLabels = ['Submissions'];
+          $scope.submissionPromise.then(function(data) {
+
+              var affsById = {};
+              var top3 = _.pairs(_.countBy(_.flatten(data.map(function(s) {
+                return s.authors.map(function(a) { affsById[a.affiliation.id] = a.affiliation; return a.affiliation});
+              })), function(a) { return a.id; })).sort(function(a, b) { return b[1] - a[1]}).splice(0,3);
+
+              $scope.subAffData = top3.map(function(a) { return [a[1]] });
+              $scope.subAffSeries = top3.map(function(a) { return affsById[a[0]].name});
+
+              $scope.subAffOptions = {
+                legend: {display: true},
+                scales: {
+                  yAxes: [{
+                    display: true,
+                    ticks: {
+                      suggestedMin: 0
+                    }
+                  }]
+                }
+              };
+
+          });
         };
+        submissionsByAffs();
 
-        $scope.subAffLabels = ['Submissions'];
-        $scope.subAffSeries = ['TUD', 'KIT', 'MIT', 'DHBW', 'SF'];
+        var submissionStatus = function() {
+          $scope.subStatusLabel = ["Draft", "Submitted", "Approved"];
+          $scope.submissionPromise.then(function(data) {
 
-        $scope.subAffOptions = {
-          legend: {display: true},
-          scales : {
-            yAxes: [{
-              display: true,
-              ticks: {
-                suggestedMin: 0
+
+
+
+            $scope.submissionPromise.then(function(s) {
+              $scope.subStatusData = _.pairs(_.countBy(s.map(function(s) { return s.status; }))).sort(function(p)
+                { return p[1];}).map(function(x) { return x[1];});
+
+            });
+
+            $scope.subStatusOptions = {
+              legend: {display: true},
+              scales: {
+                yAxes: [{
+                  display: true,
+                  ticks: {
+                    suggestedMin: 0
+                  }
+                }]
               }
-            }]
-          }
-        };
+            };
 
-        $scope.subAffData = [
-          [100],
-          [70],
-          [120],
-          [60],
-          [150]
-        ];
+          });
+        }
+        submissionStatus();
       }]);
