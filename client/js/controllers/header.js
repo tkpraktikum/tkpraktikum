6ea7be0bd7436a148549c2a007c72bb1857612d2
Markus Schanz <coksnuss@googlemail.com>
2016-09-07 21:07:26 +0200

    Removed Deadlines from header. Added phase label to breadcrumb bar

--- client/js/controllers/header.js
+++ client/js/controllers/header.js
@@ -1,28 +1,41 @@
 angular
   .module('app')
-  .controller('HeaderController', ['$scope', '$state', 'User', 'AuthService', 'ConferenceService',
-      function($scope, $state, User, AuthService, ConferenceService) {
+  .controller('HeaderController', ['$scope', '$state', '$q', 'User', 'AuthService', 'ConferenceService',
+      function($scope, $state, $q, User, AuthService, ConferenceService) {
     $scope.user = {};
     $scope.conferences = [];
     $scope.currentConferenceId = ConferenceService.getCurrentConferenceId();
     $scope.conferenceName = 'TKonference - Team Whisky';
-    $scope.deadlines = null;
+    $scope.phase = '';
 
     // Since HeaderController is not attached to a state with url parameters,
     // We need to listen for the stateChangeSuccess event to happen and check
     // whether we can extract the conferenceId from it.
     $scope.$on('$stateChangeSuccess', function (event, toState, toParams) {
       $scope.conferenceName = 'TKonference - Team Whisky';
-      $scope.deadlines = null;
+      $scope.phase = '';
 
       // Only display conference name on pages that are actually conference
       // specific, i.e. do not display it on the 'profile' page etc.
       if (toParams.conferenceId) {
-        ConferenceService.getCurrentConference().then(function (c) {
-            $scope.conferenceName = c.name;
-            if (c.submissionDeadline && c.reviewDeadline) {
-              $scope.deadlines = [c.submissionDeadline, c.reviewDeadline];
-            }
+        $q.all([
+          ConferenceService.getCurrentConference(),
+          ConferenceService.isSubmissionPhase(),
+          ConferenceService.isReviewPhase()
+        ]).then(function (promises) {
+          var conference = promises[0],
+            isSubmissionPhase = promises[1],
+            isReviewPhase = promises[2];
+
+          $scope.conferenceName = conference.name;
+
+          if (isSubmissionPhase) {
+            $scope.phase = '<span class="label label-info">Submission Phase</span>';
+          } else if (isReviewPhase) {
+            $scope.phase = '<span class="label label-info">Review Phase</span>';
+          } else {
+            $scope.phase = '';
+          }
         });
       }
     });
